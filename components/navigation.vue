506144ed39672cf2f9c16bcd45acf3e6e836c91a
Oukki <osiljamo@gmail.com>
2022-12-23 00:06:42 +0200

    Make navbar responsive

--- components/navigation.vue
+++ components/navigation.vue
@@ -1,9 +1,11 @@
 <template>
   <nav>
     <NuxtLink id="home" to="/" class="nav-button">Apoptoosi XIX</NuxtLink>
-    <NuxtLink to="/events" class="nav-button">Tapahtumat</NuxtLink>
-    <NuxtLink to="/signup" class="nav-button">Ilmoittautuminen</NuxtLink>
-    <NuxtLink to="/about" class="nav-button">Yhteystiedot</NuxtLink>
+    <div class="subnav">
+      <NuxtLink to="/events" class="nav-button">Tapahtumat</NuxtLink>
+      <NuxtLink to="/signup" class="nav-button">Ilmoittautuminen</NuxtLink>
+      <NuxtLink to="/about" class="nav-button">Yhteystiedot</NuxtLink>
+    </div>
   </nav>
 </template>
 
@@ -16,10 +18,16 @@ export default {
 <style scoped>
 nav {
   display: grid;
-  grid-template-columns: 75% repeat(3, auto);
-  height: 3em;
+  grid-template-columns: repeat(auto-fit, minmax(min(100%, 30rem), 1fr));
+  min-height: 3em;
   padding: 0 1em;
 }
+.subnav {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(clamp(33%, (30rem - 100%) * 999, 100%), 1fr));
+  row-gap: 1em;
+}
+
 .nav-button {
   font-family: 'Cinzel', serif;
   text-decoration: none;
@@ -27,15 +35,21 @@ nav {
   margin: auto 0;
   text-align: center;
   transition: 0.3s;
+  font-size: min(5vmin,1em);
 }
 .nav-button:hover {
   color: green;
 }
 #home {
-  font-size: 1.5em;
+  font-size: min(5vmin,1.5em);
   text-align: left;
 }
 .router-link-active {
   font-weight: bold;
+}
+@media all and (max-width: 991px) {
+  #home {
+    text-align: center;
+  }
 }
 </style>
