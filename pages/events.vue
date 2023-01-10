b1faa87492eecb8377dd4b96831fb2f6595516ed
Oukki <osiljamo@gmail.com>
2023-01-10 16:19:28 +0200

    Fix empty events

--- pages/events.vue
+++ pages/events.vue
@@ -1,5 +1,5 @@
 <template>
-  <ul>
+  <ul v-if="events.data.length > 0">
     <li class="events" v-for="(event, index) in events.data" :key="index">
       <event :title="event.translations[0].name" :description="event.translations[0].description"
              :image="event.image != null ? `${api_base + 'assets/' + event.image}` : ''"
@@ -10,6 +10,7 @@
              :parity=index />
     </li>
   </ul>
+  <ul v-else><p>Tapahtumat julkaistaan myöhemmin</p></ul>
 </template>
 
 <script setup>
@@ -30,5 +31,12 @@ ul {
   list-style: none;
   margin: 0;
   padding: 0;
+}
+p {
+  font-family: var(--body-font);
+  text-align: center;
+  margin: 2rem auto;
+  font-size: clamp(1rem, 3vmin,1.5rem);
+  max-width: 700px;
 }
 </style>
