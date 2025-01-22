7617fb2151009d6b5e6736adfd4bd2c97c3e2e99
Oula Siljamo <osiljamo@gmail.com>
2025-01-22 16:43:12 +0200

    Restructure project

---
+++ components/form_fields/longText.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <label :for="field_key">{{name}}<span v-if=required class="required">*</span></label>
+    <input :type="field_key === 'email' ? 'email' : 'text'" :name="field_key" :id="field_key" :placeholder="placeholder"
+           :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" :required="required">
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  field_key: String,
+  required: Boolean,
+  name: String,
+  placeholder: String,
+  modelValue: String,
+})
+const emits = defineEmits(['update:modelValue'])
+</script>
+
+<style scoped>
+label {
+  display: block;
+  text-align: center;
+  font-family: var(--body-font);
+  font-size: clamp(1rem, 3vmin, 1.2rem);
+}
+input[type=text], input[type=email] {
+  border: none;
+  font-size: clamp(1rem, 3vmin, 1.2rem);
+}
+input:focus {
+  outline: none;
+}
+
+.required {
+  vertical-align: top;
+  color: var(--nav1);
+}
+
+</style>
